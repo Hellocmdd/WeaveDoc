44a3c1ea6e04ccf2c9c6a88729fc746bb14c4a6d
Wangzhi Gang <3750483019@qq.com>
2026-04-27 14:53:23 +0800

    “第六次尝试显示预览，无法编辑，左右侧消失”

--- WeaveDoc.MarkdownEditor/src/App.axaml.cs
+++ WeaveDoc.MarkdownEditor/src/App.axaml.cs
@@ -2,76 +2,24 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using WeaveDoc.MarkdownEditor.Views;
-using System;
-using System.Threading.Tasks;
-using WeaveDoc.MarkdownEditor.Helpers;
-using Microsoft.Web.WebView2.Core;
 
 namespace WeaveDoc.MarkdownEditor
 {
     public partial class App : Application
     {
-        public static CoreWebView2Environment? WebView2Environment { get; private set; }
-
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
-
-            // 注册全局未处理异常处理器，帮助捕获运行时崩溃并写日志
-            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
-            {
-                try
-                {
-                    if (e.ExceptionObject is Exception ex)
-                        Logger.LogException(ex);
-                    else
-                        Logger.Log($"UnhandledException: {e.ExceptionObject}");
-                }
-                catch { }
-            };
-
-            TaskScheduler.UnobservedTaskException += (s, e) =>
-            {
-                try
-                {
-                    Logger.LogException(e.Exception);
-                }
-                catch { }
-            };
         }
 
         public override void OnFrameworkInitializationCompleted()
         {
-            Logger.Log("App: Starting OnFrameworkInitializationCompleted...");
-            
-            // 暂时移除 WebView2 初始化代码，以便应用程序能够启动并显示右侧预览
-            // try
-            // {
-            //     WebView2Environment = await CoreWebView2Environment.CreateAsync();
-            //     Logger.Log("WebView2 environment created successfully");
-            // }
-            // catch (Exception ex)
-            // {
-            //     Logger.LogException(ex);
-            // }
-
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                Logger.Log("App: Creating MainWindow...");
                 desktop.MainWindow = new MainWindow();
-                Logger.Log("App: MainWindow created successfully");
-                // 显示主窗口
-                desktop.MainWindow.Show();
-                Logger.Log("App: MainWindow shown");
-            }
-            else
-            {
-                Logger.Log("App: ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime");
             }
 
-            Logger.Log("App: Calling base.OnFrameworkInitializationCompleted()...");
             base.OnFrameworkInitializationCompleted();
-            Logger.Log("App: OnFrameworkInitializationCompleted completed");
         }
     }
 }
