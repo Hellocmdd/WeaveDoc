1ef44b62080ecd414325bc314039caf4109ca317
Wangzhi Gang <3750483019@qq.com>
2026-04-27 15:24:03 +0800

    “第六次尝试显示预览，无法编辑，左右回归”

--- WeaveDoc.MarkdownEditor/src/obj/Debug/net8.0/WeaveDoc.MarkdownEditor.AssemblyInfo.cs
+++ WeaveDoc.MarkdownEditor/src/obj/Debug/net8.0/WeaveDoc.MarkdownEditor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeaveDoc.MarkdownEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c0bad5bb993e13d26cc688c61abfc3be87729b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a3c1ea6e04ccf2c9c6a88729fc746bb14c4a6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeaveDoc.MarkdownEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeaveDoc.MarkdownEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
